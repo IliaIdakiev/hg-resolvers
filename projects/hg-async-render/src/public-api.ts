5f3269bc65ebed81f6665c3d00ea461a75b4b14a
Xapuu <xapu.ivanov@gmail.com>
2019-10-24 22:53:40 +0300

    Export type
    export async-render.component to the pulic-api.ts

--- projects/hg-async-render/src/public-api.ts
+++ projects/hg-async-render/src/public-api.ts
@@ -1,2 +1,3 @@
 export * from './lib/async-render.module';
 export { HG_ASYNC_RENDER, AsyncRenderResolver } from './lib/async-render-resolver';
+export { AsyncRenderComponent } from './lib/async-render/async-render.component';
