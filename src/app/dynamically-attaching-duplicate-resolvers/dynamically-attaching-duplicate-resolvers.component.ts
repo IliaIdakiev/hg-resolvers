5d5ac5094e7ae561f19d480d13785e4964a0a25e
Ilia Idakiev <ilia.idakiev@gmail.com>
2020-01-15 14:38:36 +0200

    dynamically attaching duplicate resolvers styles

--- src/app/dynamically-attaching-duplicate-resolvers/dynamically-attaching-duplicate-resolvers.component.ts
+++ src/app/dynamically-attaching-duplicate-resolvers/dynamically-attaching-duplicate-resolvers.component.ts
@@ -9,9 +9,27 @@ export class DynamicallyAttachingDuplicateResolversComponent implements OnInit {
 
   selectedUserId = null;
 
+  displayedColumns = ['username', 'email', 'name'];
+  postsDisplayedColumns = ['userId', 'title'];
+
+  showUserPostsCount = true;
+  showUserPosts = true;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  rowClickHandler(user: any) {
+    if (this.selectedUserId === user.id) { this.selectedUserId = null; return; }
+    this.selectedUserId = user.id;
+  }
+
+  toggleUsersPostsCount() {
+    this.showUserPostsCount = !this.showUserPostsCount;
+  }
+
+  toggleUsersPosts() {
+    this.showUserPosts = !this.showUserPosts;
+  }
 }
