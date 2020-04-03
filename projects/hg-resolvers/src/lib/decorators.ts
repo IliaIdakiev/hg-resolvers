587692309400b67b293678b0c4ad1d435868c288
Ilia Idakiev <ilia.idakiev@gmail.com>
2020-04-03 12:12:21 +0300

    fixes on decorator

--- projects/hg-resolvers/src/lib/decorators.ts
+++ projects/hg-resolvers/src/lib/decorators.ts
@@ -1,24 +1,38 @@
 import { ReplaySubject } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
+
 const NOT_SET = Symbol('NOT_SET');
+const SUBJ = Symbol('SUBJ');
+const OBS = Symbol('OBS');
+const CURRENT_VALUE = Symbol('CURRENT_VALUE');
+
+function _init() {
+  if (typeof this[CURRENT_VALUE] === 'undefined') { this[CURRENT_VALUE] = NOT_SET; }
+  this[SUBJ] = new ReplaySubject(1);
+  if (this[CURRENT_VALUE] !== NOT_SET) { this[SUBJ].next(this[CURRENT_VALUE]); }
+  this[OBS] = this[SUBJ].asObservable().pipe(distinctUntilChanged());
+}
 
 export function toObservable(target, key) {
-  let currentValue = NOT_SET;
-  const subject = new ReplaySubject(1);
-  if (target[key] !== undefined) { subject.next(target[key]); }
-  const observable = subject.asObservable().pipe(distinctUntilChanged());
   // tslint:disable-next-line:variable-name
-  const _init: () => void = target.ngOnInit;
-  // tslint:disable-next-line:only-arrow-functions space-before-function-paren
+  const _onInit: () => void = target.ngOnInit;
+
   target.ngOnInit = function (...args) {
-    if (_init) { _init.call(this, ...args); }
-    if (currentValue === NOT_SET) {
+    if (!this[SUBJ]) { _init.call(this); }
+
+    if (_onInit) { _onInit.call(this, ...args); }
+
+    if (this[CURRENT_VALUE] === NOT_SET) {
       console.warn('hg-resolvers (toObservable): WARNING! Provided undefined value for @Input. Did you forget to bind your property?');
     }
   };
 
   Object.defineProperty(target, key, {
-    set: (value) => { Promise.resolve().then(() => subject.next(value)); currentValue = value; },
-    get: () => observable
+    set(value) {
+      if (!this[SUBJ]) { _init.call(this); }
+      this[SUBJ].next(value);
+      this[CURRENT_VALUE] = value;
+    },
+    get() { return this[OBS]; }
   });
 }
