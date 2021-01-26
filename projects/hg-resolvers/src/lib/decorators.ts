332fd7a8aed17d142f5c2bed9ae9c6744989f7e3
Ilia Idakiev <ilia.idakiev@gmail.com>
2021-01-26 13:15:16 +0200

    to-observable fixes

--- projects/hg-resolvers/src/lib/decorators.ts
+++ projects/hg-resolvers/src/lib/decorators.ts
@@ -1,38 +1,57 @@
 import { ReplaySubject } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
 
-const NOT_SET = Symbol('NOT_SET');
-const SUBJ = Symbol('SUBJ');
-const OBS = Symbol('OBS');
-const CURRENT_VALUE = Symbol('CURRENT_VALUE');
+const state = new WeakMap();
 
-function _init() {
-  if (typeof this[CURRENT_VALUE] === 'undefined') { this[CURRENT_VALUE] = NOT_SET; }
-  this[SUBJ] = new ReplaySubject(1);
-  if (this[CURRENT_VALUE] !== NOT_SET) { this[SUBJ].next(this[CURRENT_VALUE]); }
-  this[OBS] = this[SUBJ].asObservable().pipe(distinctUntilChanged());
-}
+export function toObservable(target: any, key: any): any {
+  const NOT_SET = Symbol('NOT_SET');
+  const SUBJ = Symbol('SUBJ');
+  const OBS = Symbol('OBS');
+  const CURRENT_VALUE = Symbol('CURRENT_VALUE');
 
-export function toObservable(target, key) {
+  // tslint:disable-next-line:typedef
+  function _do(this: any, value?: any) {
+    const currentState = state.get(this) || {};
+    if (!currentState.hasOwnProperty(key)) { currentState[key] = {}; }
+
+    if (typeof currentState[key][CURRENT_VALUE] === 'undefined') {
+      currentState[key][CURRENT_VALUE] = arguments.length === 0 ? NOT_SET : value;
+      currentState[key][SUBJ] = new ReplaySubject(1);
+      currentState[key][OBS] = currentState[key][SUBJ].asObservable().pipe(distinctUntilChanged());
+      state.set(this, currentState);
+    }
+
+    if (arguments.length !== 0) { currentState[key][CURRENT_VALUE] = value; }
+    if (currentState[key][CURRENT_VALUE] !== NOT_SET) {
+      currentState[key][SUBJ].next(currentState[key][CURRENT_VALUE]);
+    }
+
+    return currentState;
+  }
   // tslint:disable-next-line:variable-name
   const _onInit: () => void = target.ngOnInit;
 
-  target.ngOnInit = function (...args) {
-    if (!this[SUBJ]) { _init.call(this); }
+  // tslint:disable-next-line:space-before-function-paren
+  target.ngOnInit = function (...args: any[]): any {
 
-    if (_onInit) { _onInit.call(this, ...args); }
+    const currentState = _do.call(this);
 
-    if (this[CURRENT_VALUE] === NOT_SET) {
+    if (_onInit) { (_onInit as any).call(this, ...args); }
+
+    if (currentState[CURRENT_VALUE] === NOT_SET) {
       console.warn('hg-resolvers (toObservable): WARNING! Provided undefined value for @Input. Did you forget to bind your property?');
     }
   };
 
   Object.defineProperty(target, key, {
+    // tslint:disable-next-line:typedef
     set(value) {
-      if (!this[SUBJ]) { _init.call(this); }
-      this[SUBJ].next(value);
-      this[CURRENT_VALUE] = value;
+      _do.call(this, value);
     },
-    get() { return this[OBS]; }
+    // tslint:disable-next-line:typedef
+    get() {
+      const currentState = state.get(this) || {};
+      return currentState[key][OBS];
+    }
   });
 }
